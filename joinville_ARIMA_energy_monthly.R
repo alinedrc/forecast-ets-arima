b549985461d9345ca427c4aa62e078dd1d4df913
Aline Cesconetto <151611609+alinedrc@users.noreply.github.com>
2025-07-29 20:55:27 -0300

    Update joinville_ARIMA_energy_monthly.R

--- joinville_ARIMA_energy_monthly.R
+++ joinville_ARIMA_energy_monthly.R
@@ -1,13 +1,15 @@
-# Graphs and metrics:  
-# Correlation and time series of residential electricity monthly consumption in Joinville with ARIMA 
+#Graphs and metrics:  
+#Correlation and time series of residential electricity monthly consumption in Joinville with ARIMA 
 
+#Loading libraries
 library(readxl)
 library(fpp3)
 
+#Loading table
 tab_cor <- read_excel("correlation_table_joinville.xlsx")
 view(tab_cor)  
 
-# Transforming into a time series  
+#Transforming into a time series  
 data <- tab_cor |>   
   select(Date, `MWh/month`) |>   
   mutate(Month = yearmonth(Date)) |>      
@@ -15,70 +17,66 @@ data <- tab_cor |>
   as_tsibble(index = Month)  
 
 # ARIMA
-# Training the model using data until June 2023
-# Forecasting for six months 
-
-# Train: Selecting data up to 2023
+#Training the model using data until december 2023
 train <- data |>  
   filter(year(Month) <= 2023) |>
   select(Month, Consumption)  
 view(train)  
 
-# Model fitting
+#Model fitting
 fit <- train |> model(ARIMA(Consumption))
 
-# Check the model output
+#Check the model output
 report(fit)
 
-# Forecasting: Predicting for 12 months ahead
+#Selecting the forecast horizon
 fc <- fit |> forecast(h = 12)
+#fc <- fit |> forecast(h = 6)
+#fc <- fit |> forecast(h = 3)
 
-# Plot the forecast (without confidence intervals)
+#Plotting the forecast without confidence intervals
 fc |>  
   autoplot(train, level = NULL) +  
   labs(y = "MWh/month")
 
-# Plot with forecast and confidence intervals
+#Plotting with forecast and confidence intervals
 fc |>
   autoplot(train) +
   labs(title = fit$`ARIMA(Consumption)`, y = "MWh/month") 
 
-# Calculating residuals and fitted values
+#Calculating residuals and fitted values
 aug <-  fit |> augment()
 
-# View the augmented data
+#View the augmented data
 head(aug)
 
-# Plot the forecast, intervals, and fitted values
+#Plotting the forecast, intervals and fitted values
 fc |>
   autoplot(data) +
   geom_line(data = aug, aes(y = .fitted, colour = .model)) + 
   guides(colour = guide_legend(title = "Series"))
 
-# Check residuals for model diagnostics
+#Checking residuals for model diagnostics
 fit |> gg_tsresiduals()
 
-# Autocorrelation test
+#Autocorrelation test
 aug |> features(.resid, box_pierce)
 
-# Accuracy metrics on training data
+#Accuracy metrics on training data
 accuracy(fit)
 
-# Accuracy on test data for model evaluation
+#Accuracy on test data for model evaluation
 accuracy(fc, data)
 
-# Correlation: Comparing real vs adjusted values
+#Correlation: Comparing real vs adjusted values
 real <- aug$Consumption  
 adjusted <- aug$.fitted
 
-# Pearson correlation test
+#Pearson correlation test
 cor.test(real, adjusted)
 
-# R² Calculation
-
+#R² Calculation
 SSE <- sum((real - adjusted)^2)
 SST <- sum((real - mean(real))^2)
-
-# R²
 R2 <- 1 - (SSE / SST)
 R2
